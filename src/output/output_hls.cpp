c7f9ea8f5bfa69ebf93541c16504415ab7933851
Eli Mallon <eli@livepeer.org>
2024-05-09 00:58:32 +0000

    onebinary: MistServer!

--- src/output/output_hls.cpp
+++ src/output/output_hls.cpp
@@ -165,7 +165,7 @@ namespace Mist{
     return result.str();
   }
 
-  OutHLS::OutHLS(Socket::Connection &conn) : TSOutput(conn){
+  OutHLS::OutHLS(Socket::Connection &conn) : TSOutputHTTP(conn){
     uaDelay = 0;
     realTime = 0;
     until = 0xFFFFFFFFFFFFFFFFull;
@@ -445,7 +445,7 @@ namespace Mist{
       return;
     }
     // Invoke the generic TS output sendNext handler
-    TSOutput::sendNext();
+    TSOutputHTTP::sendNext();
   }
 
   void OutHLS::sendTS(const char *tsData, size_t len){H.Chunkify(tsData, len, myConn);}

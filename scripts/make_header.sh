f6941cc83dbb37ec8b1c28d43162b74c5b833af4
Eli Mallon <eli@aquareum.tv>
2024-07-25 11:43:24 -0700

    meson: use wrapper to generate capabilities when cross-compiling

--- scripts/make_header.sh
+++ scripts/make_header.sh
@@ -3,8 +3,13 @@
 set -euo pipefail
 
 set +e
-json="$(./$1 -j)"
-status=$?
+if [[ "${MESON_EXE_WRAPPER}" != "" ]]; then
+  json="$($MESON_EXE_WRAPPER ./$1 -j)"
+  status=$?
+else
+  json="$(./$1 -j)"
+  status=$?
+fi
 set -e
 if [[ -z "${json// }" ]]; then
   echo 'empty json'
